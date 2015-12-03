6250d182f52e45054178aa52d154537aba49789a
BobbumMan <stephenegregg@gmail.com>
2015-12-03 11:58:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ exports.people = [
     {name: "Steve", number: "2", so:"Lauren"},
     {name: "Lauren", number: "3", so:"Steve"},
     {name: "Devin", number: "4", so:"Shelby"},
-    {name: "Josh", number: "5", so:null}
+    {name: "Josh", number: "5", so:null},
+    {name: "Josh", number: "5", so:null},
+    {name: "Josh", number: "5", so:null},
 ];
 
 exports.twilio = {
     sid: "{INSERT SID}",
     token: "{INSERT AUTH TOKEN}",
-    number: "{INSERT PHONE NUMBER}"
+    number: "{INSERT PHONE NUMBER}",
 }
